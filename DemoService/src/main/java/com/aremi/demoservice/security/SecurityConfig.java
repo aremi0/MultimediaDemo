4fa1b4366d33bf6491d8cdaf782a9f901f532a07
aremi0 <gabriele.passarello98@gmail.com>
2025-08-10 19:37:27 +0200

    Endpoint pubblici sistemati.

--- DemoService/src/main/java/com/aremi/demoservice/security/SecurityConfig.java
+++ DemoService/src/main/java/com/aremi/demoservice/security/SecurityConfig.java
@@ -24,8 +24,8 @@ public class SecurityConfig {
                         .requestMatchers("/v2/public/**").permitAll() // rotte pubbliche
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //.sessionManagement(session -> session
+                //        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(
                         oauth2 -> oauth2
                                 .jwt(jwt -> jwt
