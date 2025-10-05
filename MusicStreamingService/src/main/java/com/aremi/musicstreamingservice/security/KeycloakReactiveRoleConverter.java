6bc4a62a9a6c409d7cef7fcd819493f08110ef14
aremi0 <gabriele.passarello98@gmail.com>
2025-10-05 23:48:51 +0200

    Impostato oauth2 resource server

---
+++ MusicStreamingService/src/main/java/com/aremi/musicstreamingservice/security/KeycloakReactiveRoleConverter.java
@@ -0,0 +1,40 @@
+package com.aremi.musicstreamingservice.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Classe che si occupa di mappare i ruoli del Realm di Keycloak in ruoli compatibili con Spring
+ *
+ * Trasforma i ruoli di Keycloak (es: USER o admin-5) in GrantetAuthorities (ROLE_USER o ROLE_ADMIN_5)
+ */
+
+@Slf4j
+public class KeycloakReactiveRoleConverter implements Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+
+    @Override
+    public Mono<AbstractAuthenticationToken> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+        List<String> roles = Optional.ofNullable(realmAccess)
+                .map(r -> (List<String>) r.get("roles"))
+                .orElse(List.of());
+
+        log.info("Token roles: {}", roles);
+
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase().replace("-", "_")))
+                .collect(Collectors.toList());
+
+        return Mono.just(new JwtAuthenticationToken(jwt, authorities));
+    }
+}
+
--- MusicStreamingService/src/main/java/com/aremi/musicstreamingservice/security/SecurityConfig.java
+++ MusicStreamingService/src/main/java/com/aremi/musicstreamingservice/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.aremi.musicstreamingservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -13,8 +14,13 @@ public class SecurityConfig {
         return http
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/v1/public/**").permitAll()
-                        .anyExchange().authenticated()) // Accesso pubblico
+                        .anyExchange().authenticated()
+                )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakReactiveRoleConverter()))
+                )
                 .build();
     }
+
 }
