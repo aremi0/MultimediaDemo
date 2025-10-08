8a7d4e5d2f09f6909dd4239a10d3889d397e88ab
aremi0 <gabriele.passarello98@gmail.com>
2025-10-08 17:20:26 +0200

    Sistemato song repository

--- MusicStreamingService/src/main/java/com/aremi/musicstreamingservice/repository/SongRepository.java
+++ MusicStreamingService/src/main/java/com/aremi/musicstreamingservice/repository/SongRepository.java
@@ -4,9 +4,10 @@ import com.aremi.musicstreamingservice.model.Song;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface SongRepository extends ReactiveMongoRepository<Song, String> {
     Flux<Song> findByTitleContainingIgnoreCase(String title);
-    Flux<Song> findTopByOrderByPlayCountDesc();
+    Mono<Song> findTopByOrderByPlayCountDesc();
 }
